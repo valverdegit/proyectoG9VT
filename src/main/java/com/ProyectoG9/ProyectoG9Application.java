956b568325f467214f9d32d47299d8d166427451
valve <valve@EVA-01>
2025-07-09 10:31:14 -0600

    Iniacilazacion Proyecto

---
+++ src/main/java/com/ProyectoG9/ProyectoG9Application.java
@@ -0,0 +1,13 @@
+package com.ProyectoG9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoG9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoG9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoG9/ProyectoG9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoG9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoG9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
