829a2ea268a48f2319eefd53f17e1fd8c7b79400
valve <valve@EVA-01>
2025-07-09 11:01:19 -0600

    Base del proyecto

---
+++ src/main/java/com/ProyectoG9/ProjectConfig.java
@@ -0,0 +1,58 @@
+package com.ProyectoG9;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import java.util.Locale;
+
+       
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("HomePage");
+        registry.addViewController("/usuario").setViewName("usuario");
+        registry.addViewController("/modificacionesautos").setViewName("modificacionautos");
+        registry.addViewController("/verautos").setViewName("verautos");
+    }
+    /* El siguiente método se utilizar para publicar en la nube, independientemente  */
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+
+}
