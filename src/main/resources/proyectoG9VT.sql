3e4a7524c193857064b6b70c6b7b9ed7baf893ec
luisc <luisc@CompuLuisk.lan>
2025-07-11 11:13:25 -0600

    base de datos y fragmento de favoritos

---
+++ src/main/resources/proyectoG9VT.sql
@@ -0,0 +1,23 @@
+/*Se crea la base de datos */
+drop schema if exists proyectoG9VT;
+drop user if exists usuario;
+CREATE SCHEMA proyectoG9VT ;
+
+create user 'usuario'@'%' identified by 'clave_usuario';
+
+
+/*Se asignan los prvilegios sobr ela base de datos PoryectoG9VT al usuario creado */
+grant all privileges on techshop.* to 'usuario¿'@'%';
+flush privileges;
+
+use poryectoG9VT;
+
+/* la tabla de categoria contiene categorias de productos*/
+create table favoritos (
+  id_favoritos INT NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR(150) NOT NULL,
+  activo bool,
+  PRIMARY KEY (id_favoritos))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
